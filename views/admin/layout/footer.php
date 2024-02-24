f0579f65421e4a97db580f01764bb1758e711021
Mohamed Faalil <mohamedfaalil@gmail.com>
2024-02-24 12:35:04 +0530

    update: footer attributes are updated

--- views/admin/layout/footer.php
+++ views/admin/layout/footer.php
@@ -1,7 +1,6 @@
     <footer class="app-footer"> <!--begin::To the end-->
-        <div class="float-end d-none d-sm-inline">Anything you want</div> <!--end::To the end--> <!--begin::Copyright--> <strong>
-            Copyright &copy; 2014-2024&nbsp;
-            <a href="https://adminlte.io" class="text-decoration-none">AdminLTE.io</a>.
+            Copyright &copy; 2014-<?= \helpers\utilities\DateTimeUtility::getCurrentDate("Y") ?>&nbsp;
+            <a href="<?= url("/"); ?>" class="text-decoration-none">Steel Wall</a>.
         </strong>
         All rights reserved.
         <!--end::Copyright-->
