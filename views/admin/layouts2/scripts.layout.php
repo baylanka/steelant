1cf3d526d6f85c74f6ccb612cd3efd505da959e7
Mohamed Faalil <mohamedfaalil@gmail.com>
2024-02-24 12:14:23 +0530

    delete: layout2 directory is deleted

--- views/admin/layouts2/scripts.layout.php
+++
@@ -1,7 +0,0 @@
-<script src="<?= assets("admin/js/overlayscrollbars.browser.es6.min.js") ?>"></script>
-<script src="<?= assets("admin/js/popper.min.js") ?>"></script>
-<script src="<?= assets("admin/js/bootstrap.min.js") ?>"></script>
-<script src="<?= assets("admin/js/adminlte.js") ?>"></script>
-<script src="<?= assets("admin/js/selector.js") ?>"></script>
-<script src="<?= assets("admin/js/apexcharts.min.js") ?>"></script>
-<script src="<?= assets("admin/js/chart.js") ?>"></script>
