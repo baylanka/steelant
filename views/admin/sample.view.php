86c3db6919ca82acf6ae07b659a8ffa5319e2011
Mohamed Faalil <mohamedfaalil@gmail.com>
2024-02-24 12:35:39 +0530

    feat: sample view file for admin panel is added

---
+++ views/admin/sample.view.php
@@ -0,0 +1,13 @@
+<?php
+use helpers\translate\Translate;
+?>
+<?php require_once basePath("views/admin/layout/upper_template.php")?>
+<!--Place Your Content Here-->
+
+<?php require_once basePath("views/admin/layout/middle_template.php") ?>
+<?php require_once basePath("views/admin/layout/scripts.php"); ?>
+<script>
+    //Place your javascript logics here
+</script>
+<?php require_once basePath("views/admin/layout/lower_template.php"); ?>
+
