12660db89e0071ae28a18da40da1b7c4f9fc051c
Mohamed Faalil <mohamedfaalil@gmail.com>
2024-02-28 13:23:30 +0530

    framework_feat: storage/public directory symbolic link within public directory and remove link features are added

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@ global $app;
 
 $app->get('/test',  ["TestController", "test"]);
 $app->get('/migrate', ["AppAssistanceController", "migrate"]);
+$app->get('/add_storage_link', ["AppAssistanceController", "linkStorage"]);
+$app->get('/remove_storage_link', ["AppAssistanceController", "unlinkStorage"]);
 
 $app->get('/',  ["user\HomeController", "index"]);
 
