3f7f43238884effee52d7409c3f8d504c577e58f
Mohamed Faalil <mohamedfaalil@gmail.com>
2024-03-20 13:22:09 +0530

    temp: template filling service method test method

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -4,12 +4,25 @@ namespace controllers;
 
 use app\Request;
 use controllers\BaseController;
+use helpers\services\TemplateService;
+use model\Template;
 
 
 class TestController extends BaseController
 {
     public function test(Request $request)
     {
-
+        $data = [
+            'template-1' => TemplateService::getTemplateByFillingDataById(9, [
+                'title' => "MF-001",
+                'paras' => [12,45,49],
+            ]),
+            'template-2' => TemplateService::getTemplateByFillingDataById(9, [
+                'title' => "MBV-021",
+                'paras' => ['ASDF', ':LKJ'],
+            ])
+        ];
+        echo "<pre>";
+        print_r($data);
     }
 }
