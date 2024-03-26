40b1cb1f5bc8b269ef966d836830338e5c3717c8
mohamedsajaath <mohamedsajaath0409@gmail.com>
2024-03-26 19:14:50 +0530

    UI : pages created

--- views/admin/pages/page.view.php
+++ views/admin/pages/page.view.php
@@ -6,19 +6,26 @@ use helpers\translate\Translate;
 <?php require_once basePath("views/admin/layout/upper_template.php") ?>
 <!--Place Your Content Here-->
 
+
+<div class="w-100 d-flex justify-content-end gap-2 mb-3">
+    <button class="btn btn-sm btn-secondary align-self-start" type="button" id="create-connector">Create
+        Add-on
+    </button>
+    <button class="btn btn-sm btn-primary align-self-start" type="button" id="create-connector">Create
+        Connector
+    </button>
+
+</div>
 <div class="row">
+
     <div class="card mb-4">
 
 
-        <div class="card-header d-flex justify-content-end gap-3">
+        <div class="card-header d-flex justify-content-start gap-2">
 
-
-                <button class="btn btn-sm btn-secondary align-self-start" type="button" id="create-connector">Create
-                    Add-on
-                </button>
-                <button class="btn btn-sm btn-primary align-self-start" type="button" id="create-connector">Create
-                    Connector
-                </button>
+            <button class="btn btn-sm btn-success align-self-start" type="button" id="save-changes" disabled>
+                Save Changes
+            </button>
 
 
 
@@ -42,7 +49,7 @@ use helpers\translate\Translate;
                     <td>
                         <i class="bi bi-list"></i>
                     </td>
-                    <td>
+                    <td class="order">
                         01
                     </td>
                     <td>
@@ -68,7 +75,7 @@ use helpers\translate\Translate;
                             </button>
                             <ul class="dropdown-menu">
                                 <li>
-                                    <a href="#" class="dropdown-item" type="button" >
+                                    <a href="#" class="dropdown-item" type="button">
                                         Edit <i class="bi bi-pen float-end"></i>
                                     </a>
                                 </li>
@@ -80,7 +87,7 @@ use helpers\translate\Translate;
                     <td>
                         <i class="bi bi-list"></i>
                     </td>
-                    <td>
+                    <td class="order">
                         02
                     </td>
                     <td>
@@ -106,7 +113,7 @@ use helpers\translate\Translate;
                             </button>
                             <ul class="dropdown-menu">
                                 <li>
-                                    <a href="#" class="dropdown-item" type="button" >
+                                    <a href="#" class="dropdown-item" type="button">
                                         Edit <i class="bi bi-pen float-end"></i>
                                     </a>
                                 </li>
@@ -119,7 +126,7 @@ use helpers\translate\Translate;
                     <td>
                         <i class="bi bi-list"></i>
                     </td>
-                    <td>
+                    <td class="order">
                         03
                     </td>
                     <td>
@@ -145,7 +152,7 @@ use helpers\translate\Translate;
                             </button>
                             <ul class="dropdown-menu">
                                 <li>
-                                    <a href="#" class="dropdown-item" type="button" >
+                                    <a href="#" class="dropdown-item" type="button">
                                         Edit <i class="bi bi-pen float-end"></i>
                                     </a>
                                 </li>
@@ -159,7 +166,7 @@ use helpers\translate\Translate;
                     <td>
                         <i class="bi bi-list"></i>
                     </td>
-                    <td>
+                    <td class="order">
                         04
                     </td>
 
@@ -188,7 +195,7 @@ use helpers\translate\Translate;
                             </button>
                             <ul class="dropdown-menu">
                                 <li>
-                                    <a href="#" class="dropdown-item" type="button" >
+                                    <a href="#" class="dropdown-item" type="button">
                                         Edit <i class="bi bi-pen float-end"></i>
                                     </a>
                                 </li>
@@ -211,8 +218,36 @@ use helpers\translate\Translate;
 <?php require_once basePath("views/admin/layout/middle_template.php") ?>
 <?php require_once basePath("views/admin/layout/scripts.php"); ?>
 <script>
-    new Sortable(document.getElementById('contents'), {
-        // options here
+    let contentBody = document.getElementById('contents');
+
+    new Sortable(contentBody, {
+
+        onEnd: function (evt) {
+            console.log(contentBody.children);
+
+
+            let childrens = contentBody.children;
+
+            for (let i = 0; i < childrens.length; i++) {
+
+                console.log(i);
+                let count = i + 1;
+                childrens[i].getElementsByClassName("order")[0].textContent = count > 9 ? "" + count : "0" + count;
+
+                document.getElementById("save-changes").removeAttribute("disabled");
+            }
+
+            // contentBody.children.forEach(function (){
+            //     console.log("dsfd");
+            // });
+        }
+
+    });
+
+    $("tbody.contents").click(function () {
+
+
+        alert("sf");
     });
 </script>
 <?php require_once basePath("views/admin/layout/lower_template.php"); ?>
