421dd9438521957087a07b1011a7a4e0f3924df9
Mohamed Faalil <mohamedfaalil@gmail.com>
2024-05-20 14:24:24 +0530

    update: 2nd ad-on is added

---
+++ views/user/templates/ad-on-02.php
@@ -0,0 +1,13 @@
+<?php
+use \model\Template;
+?>
+<div class=" my-5  p-5" style="background-color: #eef1f4;">
+    <p class="custom-font">
+        <?php if($mode == Template::MODE_VIEW):?>
+            <?= $ad_on_content->description ?? '' ?>
+        <?php else: ?>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+        <?php endif; ?>
+    </p>
+
+</div>
