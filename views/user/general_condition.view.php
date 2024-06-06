a48e86a622c32bedc4f615a43aa1a838fe3a4426
Mohamed Faalil <mohamedfaalil@gmail.com>
2024-06-06 10:42:52 +0530

    feedback changes: general terms and condition page is updated

--- views/user/general_condition.view.php
+++ views/user/general_condition.view.php
@@ -40,20 +40,26 @@
 
         <h4 class="connector-heading my-3"><?=Translate::get('terms_and_conditions','legal_advices')?></h4>
 
+<!--        <div class="divider"></div>-->
+<!---->
+<!--        <h5 class="connector-heading my-3">-->
+<!--            --><?php //=Translate::get('terms_and_conditions','please_download_open_pdf_file')?>
+<!--        </h5>-->
+
         <div class="divider"></div>
 
-        <h5 class="connector-heading my-3">
-            <?=Translate::get('terms_and_conditions','please_download_open_pdf_file')?>
-        </h5>
-
-        <div class="divider"></div>
-
-
-        <div class="d-flex align-middle w-50 justify-content-between my-5">
-            <h6 class="connector-heading my-4">
+        <?php
+            $fileName = Translate::get('terms_and_conditions','general_terms_and_conditions_file');
+            $termURI = "storage/general_term_assets/" . $fileName;
+            $termURL = assets($termURI);
+        ?>
+        <div class="d-flex align-middle w-50 justify-content-between  mb-4 mt-4">
+            <h6 class="connector-heading">
                 <?=Translate::get('terms_and_conditions','general_terms_and_conditions')?>
             </h6>
-            <img src="<?= assets("themes/user/img/pdf.png") ?>" height="25" class="mt-auto mb-auto">
+            <a href="<?=$termURL?>" download="<?=$fileName?>">
+                <img src="<?= assets("themes/user/img/pdf.png") ?>" height="25" class="mt-auto mb-auto">
+            </a>
         </div>
 
         <div class="divider"></div>
